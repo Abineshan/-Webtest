47d1b2ba459cc1326ebfb32b0449ca2991f2329a
Abineshan <abineshan100@gmail.com>
2022-10-04 14:25:45 +0530

    adding files to repo

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import Navbar from './components/Navbar';
+import Side from './components/Side';
+
+function App(props) {
+  return (
+    <div className='container'>
+      <Side />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Side.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Side.css'
+
+function Side() {
+    return (
+        <div className='container'>
+            <div className='cards'>
+                <div className='card'>
+                    <div className='a'>Hello</div>
+                </div>
+                <div className='card'>
+                    <div className='a'>Hello</div>
+                </div>
+                <div className='card'>
+                    <div className='a'>Hello</div>
+                </div>
+                <div className='card'>
+                    <div className='a'>Hello</div>
+                </div>
+                <div className='card'>
+                    <div className='a'>Hello</div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Side;
